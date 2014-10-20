2237a61af2277b23044471cc51b749336706bb5f
daurnimator <quae@daurnimator.com>
2014-10-19 20:18:58 -0400

    Add notify-example

---
+++ notify-example.lua
@@ -0,0 +1,33 @@
+local cqueues = require "cqueues"
+local pgsql = require "cqueues-pgsql"
+
+local conn = pgsql.connectdb()
+if conn:status() ~= pgsql.CONNECTION_OK then
+	error(conn:errorMessage(), nil)
+end
+assert(conn:exec("LISTEN somechannel"):status() == pgsql.PGRES_COMMAND_OK)
+
+local loop = cqueues.new()
+
+loop:wrap(function()
+	while true do
+		if cqueues.poll({pollfd = conn:socket(); events = "r"}) then
+			conn:consumeInput()
+			local n = conn:notifies()
+			if n then
+				print("NOTIFIED", n:relname(), n:pid(), n:extra())
+			end
+		end
+	end
+end)
+loop:wrap(function()
+	while true do
+		if conn:exec("NOTIFY somechannel, 'hi!'"):status() ~= pgsql.PGRES_COMMAND_OK then
+			error(conn:errorMessage(), nil)
+		end
+		print("SENT NOTIFY")
+		cqueues.sleep(2)
+	end
+end)
+
+assert(loop:loop())
